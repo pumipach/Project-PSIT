4dc7b1d9cbe1bed37abba147af223901c38d4a84
KornDva <kornasak@users.noreply.github.com>
2015-12-12 22:54:53 +0700

    Update project-psit.py

--- project-psit.py
+++ project-psit.py
@@ -1,18 +1,61 @@
 import webbrowser
 import folium
+from nvd3 import multiBarChart
 
-def bulid_map_51():
-    """Buile html for thailand map."""
-# [ละติจูด,ลองติดจูด] และการซูมลำดับ 5
-map_osm = folium.Map(location=[15.0000, 100.0000], zoom_start=5)
-map_osm.polygon_marker(location=[13.72842, 100.53122], popup='โรงพยาบาลกรุงเทพคริสเตียน',
-                     fill_color='#FF0000', num_sides=3, radius=10, rotation=60)
-map_osm.create_map(path='thailandmap_51.html')
-
-year = input("ปีที่ต้องการดู(พ.ศ.) : ")
+year = input("ปีที่ต้องการดูตั้งแต่ปี 2551 - 2558(พ.ศ.) : ")
 if "51" in year:
-    bulid_map_51()
+    import year_51
+    year_51.bulid_map_51()
     # Open the page in a new browser tab
     webbrowser.open_new_tab('thailandmap_51.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "52" in year:
+    import year_52
+    year_52.bulid_map_52()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_52.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "53" in year:
+    import year_53
+    year_53.bulid_map_53()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_53.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "54" in year:
+    import year_54
+    year_54.bulid_map_54()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_54.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "55" in year:
+    import year_55
+    year_55.bulid_map_55()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_55.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "56" in year:
+    import year_56
+    year_56.bulid_map_56()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_56.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "57" in year:
+    import year_57
+    year_57.bulid_map_57()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_57.html')
+    print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
+    print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
+elif "58" in year:
+    import year_58
+    year_58.bulid_map_58()
+    # Open the page in a new browser tab
+    webbrowser.open_new_tab('thailandmap_58.html')
     print("สีแดง คือ ผู้บาดเจ็บเพศหญิง")
     print("สีแดง คือ ผู้บาดเจ็บเพศชาย")
